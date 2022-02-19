b0ac2ab05ec2d20092e4ceb540e88b52afb094dc
liltmagicbox <68154458+liltmagicbox@users.noreply.github.com>
2022-02-19 10:56:31 +0900

    atlas working
    but see, we need prevent atlasing.
    1: for light. it has different path. not alpha blending.
    2: UV wrapping over 1.0. texture defines pattern, we can't just scale uv or texture.
    if texture sized, easyly overs 4096, which is seems Android gles max tex size.

--- mesh/smdloader.py
+++ mesh/smdloader.py
@@ -1,14 +1,9 @@
 from PIL import Image
-from os.path import join, split
+from os.path import join, split, splitext
+import os
 
 #smdreader. now tris only. smd has:[ vertex-mtl ..]. same mtl, same vert.
 
-
-#mesh = SMD.load(fdir)
-
-#mesh.save_obj(fname)
-
-#meshes = []
 
 class Mesh:
 	""" python object for internal usage ..and little to glTF"""
@@ -23,27 +18,135 @@ class Mesh:
 		#resultdict = load_smd(fdir)
 		#return resultdict['meshes'] #of Mesh.
 
-	def save_obj(self, fdir):
-		sevasu
+	@classmethod
+	def save_obj(cls, meshes, fdir):
+		lines = []
+		header = "# obj created from meshes"
+		lines.append(header)
+		#o meshname
+		#v x y z
+		#vt u v
+		#vn a b c
+		#usemtl mtlname
+		#f v/vt/vn v/vt/vn v/vt/vn
+		mesh1 = meshes[0]
+		objname = mesh1.modelname
+		
+		def parse_mesh(mesh, lastlen):
+			write_method = 123 #123 or 111
+
+			lines = []
+			texname = mesh.texture.get('diffuse')
+			if not texname:
+				texname = mesh.texture.get('albedo')#maybe?
+			#---vertex
+			o_ = f"o {mesh.name}"
+			lines.append(o_)
+
+			usemtl_ = f"usemtl {texname}"
+			lines.append(usemtl_)
+
+
+			if write_method == 123:
+				vs = mesh.vert_dict['position']
+				vns = mesh.vert_dict['normal']
+				vts = mesh.vert_dict['uv']
+				for i in range( len(vs)//3 ):
+					x = vs[0+i*3]
+					y = vs[1+i*3]
+					z = vs[2+i*3]
+					v_ = f"v {x} {y} {z}"
+					lines.append(v_)
+
+					x = vns[0+i*3]
+					y = vns[1+i*3]
+					z = vns[2+i*3]
+					v_ = f"vn {x} {y} {z}"
+					lines.append(v_)
+
+					x = vts[0+i*2]
+					y = vts[1+i*2]
+					v_ = f"vt {x} {y}"
+					lines.append(v_)
+
+			elif write_method == 111:
+				vs = mesh.vert_dict['position']
+				for i in range( len(vs)//3 ):
+					x = vs[0+i*3]
+					y = vs[1+i*3]
+					z = vs[2+i*3]
+					v_ = f"v {x} {y} {z}"
+					lines.append(v_)
+
+				vts = mesh.vert_dict['uv']
+				for i in range( len(vts)//2 ):
+					u = vts[0+i*2]
+					v = vts[1+i*2]
+					vt_ = f"vt {u} {v}"
+					lines.append(vt_)
+
+				vns = mesh.vert_dict['normal']
+				for i in range( len(vns)//3 ):
+					x = vns[0+i*3]
+					y = vns[1+i*3]
+					z = vns[2+i*3]
+					vn_ = f"vn {x} {y} {z}"
+					lines.append(vn_)
+
+
+			#---faces
+
+			idx = mesh.indices
+			for i in range( len(idx)//3 ):
+				v1 = idx[0+i*3]+1+lastlen
+				v2 = idx[1+i*3]+1+lastlen
+				v3 = idx[2+i*3]+1+lastlen
+				f_ = f"f {v1}/{v1}/{v1} {v2}/{v2}/{v2} {v3}/{v3}/{v3}"
+				lines.append(f_)
+
+			lastlen = len(vs)//3
+			#print(lastlen)see 1306 406 45, we need to add.
+			return lines, lastlen
+
+		lastlen = 0
+		for mesh in meshes:
+			meshline, verylastlen = parse_mesh(mesh,lastlen)
+			lastlen+=verylastlen
+			lines.extend(meshline)
+		fullline = '\n'.join(lines)
+		with open(fdir, 'w', encoding = 'utf-8') as f:
+			f.write(fullline)
 	
 	@classmethod
 	def merge(cls, meshes):
 		new_mesh = Mesh()
+		mesh1 = meshes[0]
+		# 1,2,3, 4,5,6 is vert
+		# 0,1,2, 0,1,2 is ind
+		# 0,1,2, 3,4,5 is ind_fixed
+		# 0,1,2, len(before)+..
+		#we need to add: before points number.
+		points_before = 0
 		for mesh in meshes:
 			for key, value in mesh.vert_dict.items():
 				new_mesh.vert_dict[key].extend(value)
 
-			new_mesh.indices.extend(mesh.indices)
+			#brute way
+			for idx in mesh.indices:
+				new_mesh.indices.append(idx+points_before)			
+			points_before += len(mesh.vert_dict['position'])//3
 		
-		head = mesh.filename.split('.')[0]
-		new_mesh.name = head
-		file = f'{head}_atlas.png'
-		mesh.material['name'] = f'{head}_atlas'
-		
-		#mesh.material['shader']
-		#mesh.material['texture']['diffuse'] = 
-		for key in mesh.material['texture']:
-			mesh.material['texture'][key] = file
+		new_mesh.texture = mesh1.texture #since atlas, we do so.fine.
+		new_mesh.shader = mesh1.shader
+		new_mesh.path = mesh1.path #same, ofcourse.fine.
+		new_mesh.modelname = mesh1.modelname #if from filename, same all. fine.
+		new_mesh.name = mesh1.modelname #..is ..what? ..is no more useful. overwrite it.
+		#self.modelname = ''#is from modelname.obj
+		#self.path = '' #atlas, merge, only same path.
+		#self.name = '' #name = part of modelname.obj
+		#self.texture = {}# starting diffuse
+		#self.shader = {}#both loaded while GL runnin
+		return new_mesh
 
 
 
@@ -51,84 +154,94 @@ class Mesh:
 
 	@classmethod
 	def atlas(cls, meshes, SIZE = 2048):
-		w_before =0
-		h_before =0
-		cc=0
-		atlas = Image.new('RGBA', (SIZE,SIZE) )
-
-		#for texture in mesh.material['texture']
-
-		coord_dict = {}
-		for mesh in meshes:
-			mtl = mesh.material['name'] #if we use multiple textures,,
-			fname = f"{mtl}.png"
-			fdir = join(dirname,fname)
-			img = Image.open(fdir)
-			w,h = img.size	
-			if w_before + w <= SIZE:
-				#atlas.paste(img, (w_before,h_before ) )
-				imgxa,imgya, imgxb,imgyb = (w_before,SIZE-h_before-h, w_before+w, SIZE-h_before)
-				atlas.paste(img, (imgxa,imgya, imgxb,imgyb) )
-				cc+=1
-
-				offset_x,offset_y,width_x,width_y = w_before,h_before, w,h
-				coord_dict[mtl] = (offset_x,offset_y,width_x,width_y)
-
-				w_before+=w#note it's last line
-
-			else:
-				w_before=0
-				h_before+=h
-				if h_before + h <= SIZE:
-					#atlas.paste(img, (w_before,h_before) )
+		"""we assume all channels same size. we have 4096 over problem, too!"""
+		def save_atlas(channel, onlyone = False):
+			cc=0
+			mesh1 = meshes[0]
+			w_before =0
+			h_before =0
+			atlas = Image.new('RGBA', (SIZE,SIZE) )
+			coord_dict = {}
+			for mesh in meshes:
+				meshname = mesh.name
+				
+				fname = mesh.texture[channel]
+				fdir = join(mesh.path,fname)
+				img = Image.open(fdir)
+				w,h = img.size	
+				if w_before + w <= SIZE:
+					#atlas.paste(img, (w_before,h_before ) )
 					imgxa,imgya, imgxb,imgyb = (w_before,SIZE-h_before-h, w_before+w, SIZE-h_before)
 					atlas.paste(img, (imgxa,imgya, imgxb,imgyb) )
 					cc+=1
-					
+
 					offset_x,offset_y,width_x,width_y = w_before,h_before, w,h
-					coord_dict[mtl] = (offset_x,offset_y,width_x,width_y)
+					coord_dict[meshname] = (offset_x,offset_y,width_x,width_y)
+
+					w_before+=w#note it's last line
+
 				else:
-					print('size over!')
-			img.close()
+					w_before=0
+					h_before+=h
+					if h_before + h <= SIZE:
+						imgxa,imgya, imgxb,imgyb = (w_before,SIZE-h_before-h, w_before+w, SIZE-h_before)
+						atlas.paste(img, (imgxa,imgya, imgxb,imgyb) )
+						cc+=1
+						
+						offset_x,offset_y,width_x,width_y = w_before,h_before, w,h
+						coord_dict[meshname] = (offset_x,offset_y,width_x,width_y)
+					else:
+						print('size over!')
+				img.close()
 
-		print(cc,'of texture merged')
-		head = mesh.filename.split('.')[0]
-		file = f'{head}_atlas.png'
-		path = mesh.path
-
-		atlas.save( join(path,file) )#or running dir?
+			print(cc,'of texture merged')			
+			atlasname = f'{mesh1.modelname}_atlas___{channel}.png'
+			if onlyone: atlasname = f'{mesh1.modelname}_atlas.png'
+			path = mesh1.path
+				
+			atlas.save( join(path,atlasname) )#or running dir?
+			print(f'atlas of channel:{channel} saved at {path}')
+			return coord_dict,atlasname
 		
-		#------------phase2, uv slide
-		for mesh in meshes:
-			uvlist = mesh.vert_dict['uv']
-			mtl = mesh.material['name']
-			ox,oy, w,h = coord_dict[mtl]
+		mesh1 = meshes[0]
+		
+		for channel in mesh1.texture:
+			if len(mesh1.texture)==1: #only a channel , no need to name it.
+				coord_dict,atlasname = save_atlas(channel, True)
+			else:
+				coord_dict,atlasname = save_atlas(channel)			
 
-			new_uvlist = []
-			for i in range( len(uvlist)//2 ):
-				u = uvlist[2*i+0]
-				v = uvlist[2*i+1]				
-				new_u = ox/SIZE+ u*w/SIZE
-				new_v = oy/SIZE+ v*h/SIZE
-				new_uvlist.append(new_u)
-				new_uvlist.append(new_v)
-			mesh.vert_dict['uv'] = new_uvlist
+			#------------phase2, uv slide
+			# we have offset, w,h, we load uv array, add , and replace.
+			# ..is that all? nomatter texture N, we do once. fine.
+			#----each mesh, change UV each.
+			for mesh in meshes:
+				meshname = mesh.name
+				ox,oy, w,h = coord_dict[meshname]
+				uvlist = mesh.vert_dict['uv']
 
-			mesh.material['name']
-			mesh.atlasname = file
+				new_uvlist = []
+				for i in range( len(uvlist)//2 ):
+					u = uvlist[2*i+0]
+					v = uvlist[2*i+1]				
+					new_u = ox/SIZE+ u*w/SIZE
+					new_v = oy/SIZE+ v*h/SIZE
+					new_uvlist.append(new_u)
+					new_uvlist.append(new_v)
+				mesh.vert_dict['uv'] = new_uvlist
 
-
+				#----and replace texture. we now all same texture!
+				mesh.texture[channel] = atlasname
+		return meshes
 
 	@classmethod
 	def sort_bysize(cls, meshes):				
 		fdir_dict = {}
 		def img_height(mesh):
-			mtl = mesh.material['name']
-			fname = f"{mtl}.png"
-
+			fname = mesh.texture['diffuse']
 			fdir = join(mesh.path,fname)
 			img = Image.open(fdir)
-			#print(mtl, img.size)
+			#print( img.size)
 			w,h = img.size
 			#fdir_dict[mtl] = h
 			img.close()
@@ -140,22 +253,21 @@ class Mesh:
 
 	
 	def __init__(self):
-		self.vert_dict = {}		
-		self.vert_dict['parent_bone'] = []
+		self.modelname = ''#is from modelname.obj
+		self.path = '' #atlas, merge, only same path.
+		self.name = '' #name = part of modelname.obj
+
+		#attributes can be vary.
+		self.vert_dict = {}
 		self.vert_dict['position'] = []
 		self.vert_dict['normal'] = []
 		self.vert_dict['uv'] = []
-		self.vert_dict['weights'] = []# (str, str,str,,)
+		
 		self.indices = []
-		self.material = {} #if complex mtl, shader..texture..fine. from smd is diffuse only.
-		self.material['name'] = ''
-		self.material['shader'] = {}#idonno whether filedir ..or full string or..		
-		self.material['texture'] = {}
 
-		self.name = ''#is actually, usually by material namme.
-		self.path = ''
-		self.filename = ''
-		self.atlasname=None
+		self.texture = {}# starting diffuse
+		self.shader = {}#both loaded while GL running
+		
 
 
 
@@ -231,6 +343,7 @@ def triangles_load(lines, pointer, END = 'end'):
 
 def load_smd(fdir, END = 'end'):
 	path,file = split(fdir)
+	files = os.listdir(path)
 
 	with open(fdir,encoding='utf-8') as f:
 		lines = f.readlines()	
@@ -277,19 +390,26 @@ def load_smd(fdir, END = 'end'):
 			u = float(u)
 			v = float(v)
 
-			mesh.vert_dict['parent_bone'].append(pbone)
+			#----write data to mesh.
 			mesh.vert_dict['position'].extend([x,y,z])
 			mesh.vert_dict['normal'].extend([nx,ny,nz])
-			mesh.vert_dict['uv'].extend([u,v])
-			mesh.vert_dict['weights'].append(args)#remain thisway. args str..
+			mesh.vert_dict['uv'].extend([u,v])			
 
 		mesh.indices = indices
-		#mesh.material['texture']['diffuse'] = mtl#not here
-		mesh.material['name'] = mtl
-		mesh.name = mtl
+		#smd single png assume.fine.
+		# for file in files:
+		# 	fonly = splitext(file)[0]
+		# 	meshname = fonly.split('_')[0] #body_diffuse.jpg or body.png . not model_body_dif
+		# 	if mtl == meshname:
+		# 		print('yeah',mtl)			
+		#print( mtl+'.png' in files, mtl)
+		mesh.texture['diffuse'] = mtl+'.png'
+		
+		head, ext = splitext(file)
+		mesh.modelname = head
+		mesh.name = mtl #we have only this clue.
 		mesh.path = path
-		mesh.filename = file
-		meshes.append(mesh)
+		meshes.append(mesh)		
 
 	return_tuple = {}
 	return_tuple['meshes'] = meshes
@@ -298,119 +418,37 @@ def load_smd(fdir, END = 'end'):
 
 
 dirname = 'test'
-fname = 'ST_FA_03_CU01_conv.smd'
-#fname = 'CH_HYUM.smd'
 
+fname = 'ST_FA_03_CU01_conv.smd'
+fname = 'ST_FA_03_CU01.smd'
+#fname = 'ST_FA_03_CU01_base.smd'
+#fname = 'CH_HYUM.smd'
+#fname = 'DR_DEF_YUM_A.smd'
+
+#fname = 'DR_H02_SEP01_B.smd'
 fdir = join(dirname,fname)
 
 meshes = Mesh.from_smd(fdir)
+#for m in meshes:print(m.texture)
 #print(meshes[:5])
 meshes = Mesh.sort_bysize(meshes)
 #print(meshes[:5])
 
-meshes = Mesh.atlas(meshes)# mesh UV offset,resized.
-mesh = Mesh.merge(meshes)#atlas ed -> single mesh, finally.
+#Mesh.save_obj([meshes[0]], 'test\\harrr.obj')
+#Mesh.save_obj(meshes, 'test\\harrr.obj')
+for i in meshes:
+	print(i.name)
+#meshes = Mesh.atlas(meshes)# mesh UV offset,resized.
+#Mesh.save_obj(meshes, f'test\\xxxxx.obj')
 
+#mesh = Mesh.merge(meshes)#atlas ed -> single mesh, finally.
+#Mesh.save_obj([mesh], f'test\\{mesh.name}.obj')
 
-exit()
-
-
-#----------------------------------------------atlasing
-
-
-#----------------------------------------------atlasing
-
-
-
-#================================coord uv mixmix
-
-#{
-#	mtlname : array, idx
-#}
-
-meshes = []
-
-for pri in data_dict['triangles']:
-	mtl = pri['mtl']
-	ox,oy, w,h = coord_dict[mtl]
-	aaaa=[]
-	for vtuple in pri['vertices']: #hope idx ordered. 0 to n
-		pbone, x,y,z, nx,ny,nz, u,v, *args = vtuple #args links,bId,w
-		#aaaa.extend( [x,y,z, nx,ny,nz, u,v] )
-		pbone = int(pbone)
-		x = float(x)
-		y = float(y)
-		z = float(z)
-		nx = float(nx)
-		ny = float(ny)
-		nz = float(nz)
-		u = float(u)
-		v = float(v)
-		
-		new_u = ox/SIZE+ u*w/SIZE
-		new_v = oy/SIZE+ v*h/SIZE
-
-		#aaaa.extend( [x,y,z, nx,ny,nz, new_u, new_v] )
-		aaaa.extend( [x,y,z, new_u, new_v] )
-	vertarr = aaaa
-	idxarr = pri['indices']
-	#vertarr = np.array( aaaa , dtype='float32')
-	#idxarr = np.array( pri['indices'] , dtype='float32')
-
-	meshes.append(  {'vertices':vertarr, 'indices':idxarr, 'material':mtl }  )
-
-#('-18.88304', '-2.020091', '24.99999', '0', '0', '0', '7.00522', '0.00521559'): 0,
-#('-18.88304', '7.586056', '12.19193', '0', '0', '0', '8.32871', '0.997821'): 1,
-#('-18.88304', '7.58601', '25.00003', '0', '0', '0', '7.00522', '0.997821'): 2,
-
-#'Stage_Fashion_03_cute_001_lampB_light02': {'vertices': [3.
-
-#print(meshes)
-#[{'vertices': [0.146], 'indices':idxarr, 'material':mtl]
-
-vert_max = []
-ind_max = []
-
-for mesh in meshes:
-	vertarr = mesh['vertices']
-	indarr = mesh['indices']
-	mat = mesh['material']
-
-	vert_max.extend(vertarr)
-	ind_max.extend(indarr)
-
-
-
-
-#---------------for line iter way. we don't useit.
-# mode = 'triangles'
-# end = 'end'
-
-# modestack = []
-# mode = None
-
-# if line == 'version':
-# 	break
-# elif line == 'end':
-# 	modestack.pop()
-# 	break
-# elif line == 'nodes':
-# 	modestack.append(line)
-# 	break
-# elif line == 'skeleton':
-# 	modestack.append(line)
-# 	break
-# elif line == 'triangles':
-# 	modestack.append(line)
-# 	break
-
-# if len(modestack)==0:
-# 	break
-# else:
-# 	mode = modestack[-1]
-
-# if mode = 'triangles':
-# 	if len(line.split())==1:#assume mtl
-# 		mtl = line
-# 	else:
-# 		add_vertex(line)
+# print(dir(mesh))
+# print(mesh.vert_dict)
+# print(mesh.indices)
+# print(mesh.name)
+# print(mesh.modelname)
+# print(mesh.path)
+# print(mesh.shader)
+# print(mesh.texture)
