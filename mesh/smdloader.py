88842d900cd10c459c933c7019bad8e5e9302332
liltmagicbox <68154458+liltmagicbox@users.noreply.github.com>
2022-03-08 04:58:38 +0900

    diffuse texture obj mtl..
    seems working, but we decide not to use obj. but gltf.
    ue4 loads gltf, with static mesh , fine. with animation.
    however not sk mesh.
    it shall be fbx, from blender.

--- mesh/smdloader.py
+++ mesh/smdloader.py
@@ -31,20 +31,40 @@ class Mesh:
 		#f v/vt/vn v/vt/vn v/vt/vn
 		mesh1 = meshes[0]
 		objname = mesh1.modelname
+
+		def get_mtlname(mesh):
+			texname = mesh.texture.get('diffuse')
+			mtlname = splitext(texname)[0]
+			if 'diffuse' in mtlname:
+				mtlname = mtlname.split('diffuse')[0]
+			elif 'albedo' in mtlname:
+				mtlname = mtlname.split('albedo')[0]
+			return mtlname
 		
 		def parse_mesh(mesh, lastlen):
 			write_method = 123 #123 or 111
 
 			lines = []
+
+			mtllib_ = f"mtllib {mesh.modelname}.mtl"
+			lines.append(mtllib_)
+
 			texname = mesh.texture.get('diffuse')
 			if not texname:
 				texname = mesh.texture.get('albedo')#maybe?
 			#---vertex
 			o_ = f"o {mesh.name}"
 			lines.append(o_)
+			#if 'kaidan' in texname:
+			#	print(mesh.vert_dict['uv'])
 
-			usemtl_ = f"usemtl {texname}"
+			#for no diffuse now.
+			mtlname = get_mtlname(mesh)
+			usemtl_ = f"usemtl {mtlname}"
 			lines.append(usemtl_)
+
+			s_ = f"s 1"#off, ue4 errs no smoo..
+			lines.append(s_)
 
 
 			if write_method == 123:
@@ -116,6 +136,34 @@ class Mesh:
 		fullline = '\n'.join(lines)
 		with open(fdir, 'w', encoding = 'utf-8') as f:
 			f.write(fullline)
+
+		mtlline = []
+		for mesh in meshes:
+			mtlname = get_mtlname(mesh)		
+			
+			mtlline.append( f"newmtl {mtlname}" )
+			mtlline.append( f"Ns 333.0" )
+			mtlline.append( f"Ka 1.0 1.0 1.0" )
+			mtlline.append( f"Kd 0.8 0.8 0.8" )
+			mtlline.append( f"Ks 0.5 0.5 0.5" )
+			mtlline.append( f"Ke 0.0 0.0 0.0" )
+			mtlline.append( f"Ni 1.45" )
+			mtlline.append( f"d 1.0" )
+			mtlline.append( f"illum 2" )
+			dfile = mesh.texture['diffuse']
+			if not dfile:
+				dfile = mesh.texture['albedo']
+			mtlline.append( f"map_Kd {dfile}" )
+			mtlline.append( "" )
+
+		fullline = '\n'.join(mtlline)
+
+		if '.obj' in fdir:
+			mtldir = splitext(fdir)[0]+'.mtl'
+		else:
+			mtldir = fdir+'.mtl'
+		with open(mtldir, 'w', encoding = 'utf-8') as f:
+			f.write(fullline)
 	
 	@classmethod
 	def merge(cls, meshes):
@@ -155,48 +203,60 @@ class Mesh:
 	@classmethod
 	def atlas(cls, meshes, SIZE = 2048):
 		"""we assume all channels same size. we have 4096 over problem, too!"""
-		def save_atlas(channel, onlyone = False):
+		def save_atlas(channel, onlyonetexture = False):
 			cc=0
 			mesh1 = meshes[0]
 			w_before =0
 			h_before =0
 			atlas = Image.new('RGBA', (SIZE,SIZE) )
 			coord_dict = {}
+			#----------------phase1, eachmesh, texture merge
 			for mesh in meshes:
 				meshname = mesh.name
 				
 				fname = mesh.texture[channel]
 				fdir = join(mesh.path,fname)
-				img = Image.open(fdir)
-				w,h = img.size	
-				if w_before + w <= SIZE:
-					#atlas.paste(img, (w_before,h_before ) )
-					imgxa,imgya, imgxb,imgyb = (w_before,SIZE-h_before-h, w_before+w, SIZE-h_before)
-					atlas.paste(img, (imgxa,imgya, imgxb,imgyb) )
-					cc+=1
-
-					offset_x,offset_y,width_x,width_y = w_before,h_before, w,h
-					coord_dict[meshname] = (offset_x,offset_y,width_x,width_y)
-
-					w_before+=w#note it's last line
-
-				else:
-					w_before=0
-					h_before+=h
-					if h_before + h <= SIZE:
+				try:
+					#------------open img
+					img = Image.open(fdir)
+					w,h = img.size
+					if w_before + w <= SIZE:
+						#atlas.paste(img, (w_before,h_before ) )
 						imgxa,imgya, imgxb,imgyb = (w_before,SIZE-h_before-h, w_before+w, SIZE-h_before)
 						atlas.paste(img, (imgxa,imgya, imgxb,imgyb) )
 						cc+=1
-						
+
 						offset_x,offset_y,width_x,width_y = w_before,h_before, w,h
 						coord_dict[meshname] = (offset_x,offset_y,width_x,width_y)
+
+						w_before+=w#note it's last line
+						
+
 					else:
-						print('size over!')
-				img.close()
+						w_before=0
+						h_before+=h
+						#print(w_before,w, w+w_before,meshname)					
+						#img.show()
+						if h_before + h <= SIZE:
+							imgxa,imgya, imgxb,imgyb = (w_before,SIZE-h_before-h, w_before+w, SIZE-h_before)
+							atlas.paste(img, (imgxa,imgya, imgxb,imgyb) )
+							cc+=1
+							
+							offset_x,offset_y,width_x,width_y = w_before,h_before, w,h
+							coord_dict[meshname] = (offset_x,offset_y,width_x,width_y)
+							w_before+=w#need here too!
+						else:
+							print('size over!')
+					#print( (imgxa,imgya, imgxb,imgyb),mesh.name)
+					img.close()
+					#------------open img
+				except:
+					print('no texture:',mesh.name)
+					pass#only corrd_dict has no key of mesh.
 
 			print(cc,'of texture merged')			
 			atlasname = f'{mesh1.modelname}_atlas___{channel}.png'
-			if onlyone: atlasname = f'{mesh1.modelname}_atlas.png'
+			if onlyonetexture: atlasname = f'{mesh1.modelname}_atlas.png'
 			path = mesh1.path
 				
 			atlas.save( join(path,atlasname) )#or running dir?
@@ -205,20 +265,44 @@ class Mesh:
 		
 		mesh1 = meshes[0]
 		
+		
+
 		for channel in mesh1.texture:
 			if len(mesh1.texture)==1: #only a channel , no need to name it.
 				coord_dict,atlasname = save_atlas(channel, True)
 			else:
 				coord_dict,atlasname = save_atlas(channel)			
 
+			#-very rough way. fine. we get from last done textture
+			meshes_atlas = []
+			meshes_left = []
 			#------------phase2, uv slide
 			# we have offset, w,h, we load uv array, add , and replace.
 			# ..is that all? nomatter texture N, we do once. fine.
-			#----each mesh, change UV each.
+			#----each mesh, change UV each.			
 			for mesh in meshes:
 				meshname = mesh.name
-				ox,oy, w,h = coord_dict[meshname]
+				uvoffs = coord_dict.get(meshname)
+				if not uvoffs:
+					meshes_left.append(mesh)#we do before continue
+					continue#skip below. break breaks, pass just goes after line.
+				ox,oy, w,h = uvoffs
 				uvlist = mesh.vert_dict['uv']
+				#print(max(uvlist), mesh.name) if max(uvlist)>1.0 else 1
+
+				#---big size uv, skip atlasing
+				uvabsmax = max( max(uvlist), -min(uvlist) )
+				#print(uvabsmax,mesh.name)
+				if uvabsmax>1.01:
+					meshes_left.append(mesh)#we do before continue
+					continue
+				#---prevent clip for uv 1.001
+				elif uvabsmax>1.0 and uvabsmax<1.01:
+					#print(mesh.name)
+					uvlist_div = []
+					for uvi in uvlist:						
+						uvlist_div.append( uvi/uvabsmax )
+					uvlist = uvlist_div					
 
 				new_uvlist = []
 				for i in range( len(uvlist)//2 ):
@@ -230,9 +314,11 @@ class Mesh:
 					new_uvlist.append(new_v)
 				mesh.vert_dict['uv'] = new_uvlist
 
+				meshes_atlas.append(mesh)
 				#----and replace texture. we now all same texture!
-				mesh.texture[channel] = atlasname
-		return meshes
+				mesh.texture[channel] = atlasname		
+		#return meshes
+		return meshes_atlas, meshes_left
 
 	@classmethod
 	def sort_bysize(cls, meshes):				
@@ -240,11 +326,18 @@ class Mesh:
 		def img_height(mesh):
 			fname = mesh.texture['diffuse']
 			fdir = join(mesh.path,fname)
-			img = Image.open(fdir)
-			#print( img.size)
-			w,h = img.size
-			#fdir_dict[mtl] = h
-			img.close()
+
+			try:
+				#--------img
+				img = Image.open(fdir)
+				#print( img.size)
+				w,h = img.size
+				#fdir_dict[mtl] = h
+				img.close()
+				#--------img
+			except:
+				h=123
+
 			return h
 		sorted_meshes = sorted(meshes, key=img_height )
 		return sorted_meshes
@@ -417,10 +510,10 @@ def load_smd(fdir, END = 'end'):
 
 
 
-dirname = 'test'
+dirname = 'tutuL_Cool_00'
 
-fname = 'ST_FA_03_CU01_conv.smd'
-fname = 'ST_FA_03_CU01.smd'
+#fname = 'ST_FA_03_CU01_conv.smd'
+fname = 'L_Cool_00_bg.smd'
 #fname = 'ST_FA_03_CU01_base.smd'
 #fname = 'CH_HYUM.smd'
 #fname = 'DR_DEF_YUM_A.smd'
@@ -436,13 +529,34 @@ meshes = Mesh.sort_bysize(meshes)
 
 #Mesh.save_obj([meshes[0]], 'test\\harrr.obj')
 #Mesh.save_obj(meshes, 'test\\harrr.obj')
-for i in meshes:
-	print(i.name)
+
+#for i in meshes:
+#	print(i.name)
+
 #meshes = Mesh.atlas(meshes)# mesh UV offset,resized.
 #Mesh.save_obj(meshes, f'test\\xxxxx.obj')
 
 #mesh = Mesh.merge(meshes)#atlas ed -> single mesh, finally.
 #Mesh.save_obj([mesh], f'test\\{mesh.name}.obj')
+
+
+#we merge only atlased. fine. and after save input is list!
+ated,noted = Mesh.atlas(meshes)# mesh UV offset,resized.
+mesh_ated = Mesh.merge(ated)
+
+#--------if you want atlasted and left
+wanna_save_all = True
+
+if wanna_save_all:
+	noted.append(mesh_ated)
+	Mesh.save_obj(noted, f'{dirname}\\{mesh_ated.name}.obj')
+else:	
+	Mesh.save_obj([mesh_ated], f'{dirname}\\{mesh_ated.name}.obj')
+
+
+print('saved : ',f'test\\{mesh_ated.name}.obj')
+
+
 
 # print(dir(mesh))
 # print(mesh.vert_dict)
