fb2b1e87e509c232cc3302fa290841cfb01d1fb4
liltmagicbox <68154458+liltmagicbox@users.noreply.github.com>
2022-04-30 00:32:25 +0900

    window with shader, vao,vbo
    draw-able.
    but too messy.
    
    anyway we can see system structure working.

--- objects/window.py
+++ objects/window.py
@@ -71,7 +71,7 @@ class Window:
 
     def input(self, events):
         1#print(events)
-    def update(self,dt):
+    def update(self, dt):
         1
     def draw(self):
         1
@@ -81,7 +81,6 @@ class Window:
         while not glfwWindowShouldClose(window):
             #input, update(ai,physics), draw 3-type.
             #---1 input
-            glfwPollEvents()#this , is the input!
             self.input(self.events)
             self.events = []
 
@@ -92,42 +91,238 @@ class Window:
             self.update(dt)
             
             #---3 draw
-            self.draw()
+            glClearColor(0, 0, 0, 1)
+            glClear(GL_COLOR_BUFFER_BIT)
+            self.draw()#[]=empty world
             # Swap front and back buffers
             glfwSwapBuffers(window)
+            glfwPollEvents()#this , is the input! but to next time!
 
         glfwTerminate()#This function destroys all remaining windows and cursors, 
 
 
+
+
+
+
+#low level objects
+#we need vao, atleast.
+#..but also shader.ha..
+#..we do shader.
+from OpenGL.GL import *
+from OpenGL.GL import shaders
+
+vertn = """
+#version 410 core
+layout (location = 0) in vec3 pos;
+layout (location = 1) in vec3 color;
+out vec3 ocolor;
+void main() 
+{
+    gl_Position = vec4( pos, 1);
+    ocolor = color;
+}
+"""
+
+fragn = """
+#version 410 core
+in vec3 ocolor;
+out vec4 FragColor;
+void main()
+{
+    FragColor = vec4(1,1,0,0);
+}
+"""
+def shasha():
+    assert bool(glCreateShader)#sometimes compile error occurs, before window() 
+    vshader = shaders.compileShader( vertn, GL_VERTEX_SHADER)
+    fshader = shaders.compileShader( fragn, GL_FRAGMENT_SHADER)
+    program = shaders.compileProgram( vshader,fshader)
+    glUseProgram(program)
+    glPointSize(10)
+import numpy as np
+
+
+class Vbo:
+    def __init__(self, pos):
+        VBO = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, VBO)
+        vertices = np.array([0,0,0, 0.5,0,0, 0,0.5,0]).astype('float32')
+        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+        fsize = np.float32(0.0).nbytes
+        stride = 3 * fsize
+        offset = ctypes.c_void_p(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, offset)
+        glEnableVertexAttribArray(0)
+        self.VBO = VBO
+    def draw(self):
+        glBindBuffer(GL_ARRAY_BUFFER, self.VBO)
+        glDrawArrays(GL_POINTS, 0,3)
+
+class Vao:
+    def __init__(self, pos, color=None):
+        vertices = np.array( [0,0,0, 0.5,0,0, 0,0.5,0] ).astype('float32')
+        indices = np.array( [0,1,2] ).astype('uint')#not uint8 but uint. since:GL_UNSIGNED_INT
+        
+        VAO = glGenVertexArrays(1) # create a VA. if 3, 3of VA got. #errs if no window.
+        VBO = glGenBuffers(1) #it's buffer, for data of vao.fine.
+        EBO = glGenBuffers(1) #indexed, so EBO also. yeah.
+        glBindVertexArray(VAO) #gpu bind VAO
+        glBindBuffer(GL_ARRAY_BUFFER, VBO) #gpu bind VBO in VAO
+        glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices, GL_STATIC_DRAW)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO)
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.nbytes, indices, GL_STATIC_DRAW)
+
+        # fsize = np.float32(0.0).nbytes
+        # stride = 3 * fsize
+        # offset = ctypes.c_void_p(0)
+        # glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, offset)
+        # glEnableVertexAttribArray(0)
+
+        #this only for location 0. not works to 1..
+        attrs = {'position':vertices}
+        points = len(attrs['position'])//3
+        fsize = np.float32(0.0).nbytes #to ensure namespace-safe.        
+        offset = ctypes.c_void_p(0)
+        loc = 0
+        stride = 3* fsize
+        for data_array in attrs.values():
+            data_len = len(data_array)
+            size = data_len//points#size 2,3,4
+            #loc = glGetAttribLocation(shader.ID, attrname)
+            glVertexAttribPointer(loc, size, GL_FLOAT, GL_FALSE, stride, offset)
+            glEnableVertexAttribArray(loc)
+            offset = ctypes.c_void_p(data_len*fsize)
+            loc+=1
+
+        self.VAO = VAO
+        self.VBO = VBO
+        self.EBO = EBO
+        self.points = len(indices)
+    def draw(self):        
+        glBindVertexArray(self.VAO)
+        glDrawElements(GL_POINTS, 3, GL_UNSIGNED_INT, None)
+
+
+
+
+
+
+#______high level objects
+
 class Inputmanager:
-    def __init__(self):
+    def __init__(self):#init without window, maybe we simulate events.!
         1
     def input(self, events):
+        """actual process inputs(events)"""
         for i in events:
             print(i)
     def bind(self, window):
+        """let the window bound"""
         window.input = self.input
+
+class World:
+    def __init__(self):
+        self.actors = []
+    def update(self, dt):
+        if int(dt%2)==0:
+            print(dt)
+    def bind(self, window):
+        window.update = self.update
+
+class Renderer:
+    def __init__(self):
+        self.targets = []
+    def render(self, target, camera=None):
+        """set render target
+        all inputs become draw-ready objects."""
+        shasha()#init shader here..?
+        
+        if hasattr(target, 'maincam'):
+            camera = target.maincam
+        if camera == None:
+            camera = 1#Camera()
+
+
+        #if isinstance(target, World): not this strict way.
+        if isinstance(target, World):
+            for i in target.actors:
+                self.targets = []
+
+        if isinstance(target, list):
+            vao = Vbo(target)
+            self.targets = [vao]
+
+    def draw(self):
+        """real draw. set MVP.."""
+        for i in self.targets:
+            i.draw()
+        print('i rendered')
+    def bind(self, window):
+        """window runs and calls this"""
+        window.draw = self.draw
+
+
+
+
+
+
+
+
+
 
 def main():
     w = Window('w1')
     
-    #inputmanager = Inputmanager(window)
-    #inputmanager.input(window)
+    #---old but independant way
     def custom_input_function(events):
         for e in events:
             if e['key']==256:#we need class not dict.
                 w.close()
-
     #w.input = lambda y:print(y)
     #w.input = custom_input_function
 
+    #--from the concept
+    #inputmanager = Inputmanager(window)
+    #inputmanager.input(window)
+    #--we arrived here. see input changed bind.
     i = Inputmanager()
     #w.input = i.input
     i.bind(w)#thats the way!
+    #means i.get_events_from(w)
+    #and i shall send events to ..controller? world? world seems fair.
+    
+
+    world = World()
+    #world.update(dt) #not thisway.
+    world.bind(w)
+    #means world.get_dt_from(w)
+    #and simulated. fine. no need more.
 
 
-    #r = Renderer()
+
+    r = Renderer()#..holds mvp bind kinds. it shall not be in world nor window.yeah.
+    r.bind(w)
     #r.render(world)
+    r.render([0,0,0, 0.5,0,0, 0,0.5,0])
+
+    #def mydraw():
+        #set mvp
+        #vao = vao()
+        #vao.bind()
+        #vao.draw()
+    #render.draw = mydraw
+    
+    #r.render('ham')
+    #r.get_world_from(w) or actors. or mesh or vao or [0,0,0, 1,0,0, 0,1,0]..?
+    #NO!!! render requires world, but not from window.
+    #r.render(world)#maybe, world or [actors] or kinds.
+    #r.render([0,0,0, 1,0,0, 0,1,0])#yeah. default cam 0,0,-2?
+
+    #basically we want []s, ,means we need actor class not [?].
+    
+    #and, window draw is performed by r.
+    #window holds world, for simulation.
 
     w.run()
 if __name__ == "__main__":
