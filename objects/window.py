dfc64aa37d943c444b6db290b596928fe4dd7993
liltmagicbox <68154458+liltmagicbox@users.noreply.github.com>
2022-04-30 18:57:29 +0900

    try window hint
    clear f.buffer requires more ability.
    full scr if not None
    version seems not that changing..
    how to ES?
    hopely we not use multiwindow,. too complex input.
    dt is safe, 2000s for 20ms> drift.

--- objects/window.py
+++ objects/window.py
@@ -10,6 +10,10 @@ def see_dirs():
     #print(help(glfw.get_time))
     #print(help(glfwGetTime))
 
+#https://www.glfw.org/docs/3.3/group__init.html#ga110fd1d3f0412822b4f1908c026f724a
+#but seems only joystick hat..
+#glfwInitHint(GLFW_VERSION_MAJOR, 4)
+#glfwInitHint(GLFW_VERSION_MINOR, 2)
 
 # Initialize the library
 if not glfwInit():#need thread safe, run by mainthread.
@@ -18,9 +22,58 @@ if not glfwInit():#need thread safe, run by mainthread.
 class Window:
     """single window design."""
     def __init__(self, windowname = 'a window'):
-        window = glfwCreateWindow(640, 480, windowname, None, None)
+        #hits int or str. do before create window.!
+        #glfwWindowHint
+        #glfwWindowHintString
+        #glfwDefaultWindowHints
+
+        print(GLFW_VERSION_MAJOR)
+        print(GLFW_VERSION_MINOR)
+        print(GLFW_VERSION_REVISION)
+        #https://learnopengl.com/Getting-started/Hello-Window
+        #https://www.glfw.org/docs/3.3/window_guide.html
+        #https://kyoungwhankim.github.io/ko/blog/opengl_window/
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4)
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3)
+        #glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE)        
+        #glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE)#compat works.
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE)#compat works.
+        #https://stackoverflow.com/questions/58022707/glvertexattribpointer-raise-gl-invalid-operation-version-330
+        #Since you use a core profile ... the default Vertex Array Object 0 is not valid
+        #i see.. compatibility,, 0 is automatically set.
+
+        #If requesting an OpenGL version below 3.2, GLFW_OPENGL_ANY_PROFILE must be used.
+        #If OpenGL ES is requested, this hint is ignored.
+        
+        #glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2)
+        #'Context profiles are only defined for OpenGL version 3.2 and above'
+        print(GLFW_CONTEXT_VERSION_MAJOR)
+        glfwWindowHint(GLFW_RESIZABLE,GLFW_FALSE)
+        glfwWindowHint(GLFW_FLOATING, GLFW_TRUE)
+        glfwWindowHint(GLFW_TRANSPARENT_FRAMEBUFFER, GLFW_TRUE)
+        #glfwWindowHint(GLFW_CENTER_CURSOR , GLFW_TRUE)
+        #glfwWindowHint(GLFW_DECORATED , GLFW_FALSE)#cool!
+        
+        monitor1 = glfwGetPrimaryMonitor()
+        monitor1 = None
+        window = glfwCreateWindow(640, 480, windowname, monitor1, None)
+        #https://www.glfw.org/docs/3.3/window_guide.html
+        print(glfwGetError())
+        print(glfwGetVersion())
+        s = glfwGetVersionString()
+        print(s)
+
+        
+
         if not window:
             raise Exception('no window created!')
+        glfwMakeContextCurrent(window)
+        # tell GLFW to capture our mouse
+        #glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED)
+        #glEnable(GL_DEPTH_TEST)
+        #glClearColor(0.1, 0.1, 0.1, 1.0)
+        #glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
         self.window = window
         self.events=[]
         self.set_current()
@@ -31,6 +84,7 @@ class Window:
     def close(self):
         window = self.window
         glfwSetWindowShouldClose(window,True)
+        #glfwDestroyWindow
     def set_current(self):
         window = self.window
         glfwMakeContextCurrent(window)
@@ -130,7 +184,8 @@ in vec3 ocolor;
 out vec4 FragColor;
 void main()
 {
-    FragColor = vec4(1,1,0,0);
+    FragColor = vec4(1,1,0,1);
+    //FragColor = vec4(0,0,0,0);
 }
 """
 def shasha():
@@ -152,8 +207,8 @@ class Vbo:
         fsize = np.float32(0.0).nbytes
         stride = 3 * fsize
         offset = ctypes.c_void_p(0)
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, offset)
         glEnableVertexAttribArray(0)
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, stride, offset)
         self.VBO = VBO
     def draw(self):
         glBindBuffer(GL_ARRAY_BUFFER, self.VBO)
@@ -184,14 +239,14 @@ class Vao:
         points = len(attrs['position'])//3
         fsize = np.float32(0.0).nbytes #to ensure namespace-safe.        
         offset = ctypes.c_void_p(0)
-        loc = 0
+        loc = 0#NOTE:opengl core no attr 0.
         stride = 3* fsize
         for data_array in attrs.values():
             data_len = len(data_array)
             size = data_len//points#size 2,3,4
             #loc = glGetAttribLocation(shader.ID, attrname)
-            glVertexAttribPointer(loc, size, GL_FLOAT, GL_FALSE, stride, offset)
             glEnableVertexAttribArray(loc)
+            glVertexAttribPointer(loc, size, GL_FLOAT, GL_FALSE, stride, offset)
             offset = ctypes.c_void_p(data_len*fsize)
             loc+=1
 
@@ -212,13 +267,17 @@ class Vao:
 
 class Inputmanager:
     def __init__(self):#init without window, maybe we simulate events.!
-        1
+        self.window = None
     def input(self, events):
         """actual process inputs(events)"""
         for i in events:
+            if '256' in str(i):
+                if self.window:
+                    self.window.close()
             print(i)
     def bind(self, window):
         """let the window bound"""
+        self.window = window
         window.input = self.input
 
 class World:
@@ -233,13 +292,20 @@ class World:
 class Renderer:
     def __init__(self):
         self.targets = []
+    #def render(self, world_or_actors_or_target, camera=None, window = None, pos_aabb=None):
     def render(self, target, camera=None):
-        """set render target
+        """add render target
         all inputs become draw-ready objects."""
-        shasha()#init shader here..?
+        #world,maybe just 1 simulated,.. if you can multi world, but dt takes time..
+        #cam that sees world
+        #window that draws from camera.
+
+
+        
+        shasha()#init shader here..? course not! its for temp.
         
         if hasattr(target, 'maincam'):
-            camera = target.maincam
+            camera = target.camera[0]
         if camera == None:
             camera = 1#Camera()
 
